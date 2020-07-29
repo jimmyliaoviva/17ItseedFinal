56d38a6cb3448ddd4e5decd67a93fe9d78e77c40
jimmyliaviva <jimmy.liao86@gmail.com>
2020-07-30 01:27:09 +0800

    try

--- src/scripts/pages/project_recruit.js
+++ src/scripts/pages/project_recruit.js
@@ -1,1 +1,10 @@
 import "bootstrap"
+import $ from 'jquery';
+
+$(document).ready(function() {
+
+    if(window.location.href.indexOf('#web') != -1) {
+      $('#web').modal('show');
+    }
+  
+  });
