0e115874e5b806a8b85388819a5825b89e74b7af
jimmyliaviva <jimmy.liao86@gmail.com>
2020-07-30 18:07:54 +0800

    add prev and next btn on recruit

--- src/scripts/pages/project_career.js
+++ src/scripts/pages/project_career.js
@@ -8,3 +8,11 @@ $('.learnedBtn').click(function () {
 $('.presentationBtn').click(function () {
     location.href = 'https://trello.com/invite/b/6ihc8Ht0/fc0ddaed072c241ec861e3164e2b283b/%E8%81%B7%E6%B6%AF%E6%88%90%E7%99%BC%E6%A8%A1%E7%AF%84%E5%A5%BD%E6%A3%92%E6%A3%92%E7%89%88';
 })
+
+$(document).ready(function() {
+
+    if(window.location.href.indexOf('#web') != -1) {
+      $('#web').modal('show');
+    }
+  
+  });
