2cd9d1ed05894ec62ed8e62958790c78b994eb58
jimmyliaviva <jimmy.liao86@gmail.com>
2020-08-01 00:29:50 +0800

    TB version 1

--- src/scripts/pages/project_tb.js
+++ src/scripts/pages/project_tb.js
@@ -1,1 +1,120 @@
 import 'bootstrap';
+import 'jquery';
+
+$('.danceKing').click(function () {
+    if ($(this).attr('src') == './images/tb/danceKing.jpg') {
+        $(this).attr('src', './images/tb/danceKing.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/danceKing.jpg')
+    }
+        
+})
+
+$('.fanny').click(function () {
+    if ($(this).attr('src') == './images/tb/fanny.jpg') {
+        $(this).attr('src', './images/tb/fanny.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/fanny.jpg')
+    }
+        
+})
+
+$('.ray').click(function () {
+    if ($(this).attr('src') == './images/tb/ray.jpg') {
+        $(this).attr('src', './images/tb/ray.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/ray.jpg')
+    }
+        
+})
+
+$('.min').click(function () {
+    if ($(this).attr('src') == './images/tb/min.jpg') {
+        $(this).attr('src', './images/tb/min.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/min.jpg')
+    }
+        
+})
+
+$('.newone').click(function () {
+    if ($(this).attr('src') == './images/tb/newone.jpg') {
+        $(this).attr('src', './images/tb/newone.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/newone.jpg')
+    }
+        
+})
+
+$('.pl').click(function () {
+    if ($(this).attr('src') == './images/tb/pl.jpg') {
+        $(this).attr('src', './images/tb/pl.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/pl.jpg')
+    }
+        
+})
+
+$('.bg').click(function () {
+    if ($(this).attr('src') == './images/tb/bg.jpg') {
+        $(this).attr('src', './images/tb/bg.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/bg.jpg')
+    }
+        
+})
+
+$('.ls').click(function () {
+    if ($(this).attr('src') == './images/tb/ls.jpg') {
+        $(this).attr('src', './images/tb/ls.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/ls.jpg')
+    }
+        
+})
+
+$('.july').click(function () {
+    if ($(this).attr('src') == './images/tb/july.jpg') {
+        $(this).attr('src', './images/tb/july.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/july.jpg')
+    }
+        
+})
+
+$('.lg').click(function () {
+    if ($(this).attr('src') == './images/tb/lg.jpg') {
+        $(this).attr('src', './images/tb/lg.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/lg.jpg')
+    }
+        
+})
+
+$('.jerry').click(function () {
+    if ($(this).attr('src') == './images/tb/jerry.jpg') {
+        $(this).attr('src', './images/tb/jerry.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/jerry.jpg')
+    }
+})
+
+$('.andy').click(function () {
+    if ($(this).attr('src') == './images/tb/andy.jpg') {
+        $(this).attr('src', './images/tb/andy.gif')
+    }
+    else {
+        $(this).attr('src', './images/tb/andy.jpg')
+    }
+})
